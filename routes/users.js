6c6d8fd0a495ac34463d247725b4abfb87229fa7
J <59581793+daechi73@users.noreply.github.com>
2024-02-20 21:26:14 -0500

    routed users; implemented signIn system

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/usersController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userController.user_get_users);
+router.get("/sign_in", userController.user_signIn_post);
 
 module.exports = router;
