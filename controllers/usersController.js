e76a1c2090a8bfce4faf7fa7d65bfc3ac26900b2
J <59581793+daechi73@users.noreply.github.com>
2024-02-21 13:03:47 -0500

    users_sign_up implemented

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const passport = require("passport");
-const { body, validationResult } = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Hash = require("../public/javascript/Hash.js");
 
 exports.user_get_users = asyncHandler(async (req, res, next) => {
@@ -22,5 +22,51 @@ exports.user_sign_in = [
         return res.json({ status: "success", user: user });
       });
     })(req, res, next);
+  }),
+];
+
+exports.user_sign_up = [
+  body("username")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a username")
+    .isLength({ min: 4 })
+    .withMessage("Username must be longer than 4 letters")
+    .isLength({ max: 30 })
+    .withMessage("Username must be less than 30 letters"),
+  body("name")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a name")
+    .isLength({ max: 30 })
+    .withMessage("name must be less than 30 letters"),
+  body("password")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a password")
+    .isLength({ max: 100 })
+    .withMessage("Password must be less than 50 letters"),
+  body("bio")
+    .trim()
+    .isLength({ max: 400 })
+    .withMessage("Password must be less than 50 letters"),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const user = new User({
+      user_name: req.body.username,
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+      bio: req.body.bio,
+    });
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: "failed",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    await user.save();
+    res.json({ status: "success", user: user });
   }),
 ];
