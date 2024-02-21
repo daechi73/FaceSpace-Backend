f8344d44d521b7fd020698d1a439c5aba6a74768
J <59581793+daechi73@users.noreply.github.com>
2024-02-21 13:05:51 -0500

    routed /sign_up

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/usersController");
 /* GET users listing. */
 router.get("/", userController.user_get_users);
 router.post("/sign_in", userController.user_sign_in);
+router.post("/sign_up", userController.user_sign_up);
 
 module.exports = router;
