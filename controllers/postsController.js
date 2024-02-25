15fedf1d37e7e1052214a3e91f95cb81a9719980
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 17:59:35 -0500

    posts get list

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,5 +2,18 @@ const asyncHandler = require("express-async-handler");
 const Post = require("../models/Post");
 
 exports.posts_get_posts = asyncHandler(async (req, res, next) => {
-  res.send("posts get working");
+  const posts = await Post.find().exec();
+  if (posts.length !== 0) {
+    return res.json({
+      status: "success",
+      posts: posts,
+    });
+  } else {
+    return res.json({
+      status: "failed",
+      msg: "No posts available",
+    });
+  }
 });
+
+exports.posts_post_posts = [asyncHandler(async (req, res, next) => {})];
