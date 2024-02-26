a4c17b1423a58a0447328fb172bcb242f252510a
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 20:12:00 -0500

    posts_post_posts implemented

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Post = require("../models/Post");
+const { body, validationResult } = require("express-validator");
 
 exports.posts_get_posts = asyncHandler(async (req, res, next) => {
   const posts = await Post.find().exec();
@@ -16,4 +17,24 @@ exports.posts_get_posts = asyncHandler(async (req, res, next) => {
   }
 });
 
-exports.posts_post_posts = [asyncHandler(async (req, res, next) => {})];
+exports.posts_post_posts = [
+  body("post")
+    .exists({ values: "falsy" })
+    .withMessage("You must have some input to submit a post"),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: "failure",
+        msg: "You must have some input to submit a post",
+      });
+    }
+    const post = new Post({
+      posted_user: req.body.user,
+      post_content: req.body.post,
+    });
+    console.log(post);
+    await post.save();
+    res.json({ status: "success", msg: "Post successfully added" });
+  }),
+];
