a266045981d59439a1dc60a76cfcefde6375c9bd
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 20:12:12 -0500

    post_post_posts routed

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require("../controllers/postsController");
 
 router.get("/", postsController.posts_get_posts);
+router.post("/post", postsController.posts_post_posts);
 
 module.exports = router;
