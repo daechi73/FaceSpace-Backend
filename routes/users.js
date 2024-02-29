00faa07fd3a132844f3136946f599e5fcc3ec35c
J <59581793+daechi73@users.noreply.github.com>
2024-02-28 23:47:03 -0500

    routes user_get_users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const userController = require("../controllers/usersController");
 
 /* GET users listing. */
+
 router.get("/", userController.user_get_users);
+router.get("/:id", userController.user_get_user_detail);
 router.post("/sign_in", userController.user_sign_in);
 router.post("/sign_up", userController.user_sign_up);
 
