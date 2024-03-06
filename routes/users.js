1b1452560de1a5387ee721970f0ace4934c4e84c
J <59581793+daechi73@users.noreply.github.com>
2024-03-05 21:28:09 -0500

    routed addFriend

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,10 @@ const userController = require("../controllers/usersController");
 
 router.get("/", userController.user_get_users);
 router.get("/sign_out", userController.user_sign_out);
-
 router.get("/:id", userController.user_get_user_detail);
 router.post("/other_users", userController.user_get_other_users);
 router.post("/sign_in", userController.user_sign_in);
 router.post("/sign_up", userController.user_sign_up);
+router.post("/:id/update/addFriend", userController.user_update_add_friend);
 
 module.exports = router;
