9ce83f0ad476cee0474eee6bbde893171c6b070f
J <59581793+daechi73@users.noreply.github.com>
2024-03-05 21:33:40 -0500

    working on user_update_add_friend

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,7 +16,8 @@ exports.user_get_users = asyncHandler(async (req, res, next) => {
 exports.user_get_other_users = asyncHandler(async (req, res, next) => {
   const users = await User.find()
     .where("user_name")
-    .ne(req.body.user.user_name);
+    .ne(req.body.user.user_name)
+    .select("-password");
   if (users) res.json({ status: "success", users: users });
   else res.json({ status: "faliure" });
 });
@@ -115,3 +116,76 @@ exports.user_sign_up = [
     res.json({ status: "success", user: user });
   }),
 ];
+
+exports.user_update = [
+  (req, res, next) => {
+    if (!(req.body.friends instanceof Array)) {
+      if (typeof req.body.friends === "undefined") req.body.friends = [];
+      else req.body.friends = new Array(req.body.friends);
+    }
+    next();
+  },
+  body("username")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a username")
+    .isLength({ min: 4 })
+    .withMessage("Username must be longer than 4 letters")
+    .isLength({ max: 30 })
+    .withMessage("Username must be less than 30 letters"),
+  body("password")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be longer than 8 letters")
+    .isLength({ max: 100 })
+    .withMessage("Password must be less than 50 letters"),
+  body("name")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter a name")
+    .isLength({ max: 30 })
+    .withMessage("name must be less than 30 letters"),
+  body("email")
+    .trim()
+    .exists({ values: "falsy" })
+    .withMessage("You must enter an email")
+    .isEmail()
+    .withMessage("Must input a valid email address")
+    .isLength({ max: 40 })
+    .withMessage("email must be less than 40 letters"),
+  body("bio")
+    .trim()
+    .isLength({ max: 400 })
+    .withMessage("Password must be less than 50 letters"),
+  body("friends").escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ status: "failed", errors: errors.array() });
+    }
+    const newUser = new User({
+      user_name: req.body.username,
+      password: req.body.password,
+    });
+  }),
+];
+exports.user_update_add_friend = asyncHandler(async (req, res, next) => {
+  const [signedInUser, toAddUser] = await Promise.all([
+    User.findById(req.params.id).exec(),
+    User.findById(req.body.toAddUserId).exec(),
+  ]);
+
+  if (!signedInUser) {
+    if (!toAddUser) return res.json("Both Users are not found");
+    return res.json("User doesn't exist");
+  }
+  if (!toAddUser) return res.json("user to add cannot be found in database");
+
+  signedInUser.friend_requests_outgoing.push(toAddUser);
+  toAddUser.friend_requests_incoming.push(signedInUser);
+  console.log(signedInUser);
+  console.log(toAddUser);
+  res.json(signedInUser);
+});
