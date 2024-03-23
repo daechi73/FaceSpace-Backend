44e1f456460c470bfdaa78035911bf39ed8478c0
J <59581793+daechi73@users.noreply.github.com>
2024-03-23 19:07:12 -0400

    updated changed middleware function name

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,9 @@ router.get("/:id", userController.user_get_user_detail);
 router.post("/other_users", userController.user_get_other_users);
 router.post("/sign_in", userController.user_sign_in);
 router.post("/sign_up", userController.user_sign_up);
-router.post("/:id/update/addFriend", userController.user_update_add_friend);
+router.post(
+  "/:id/update/addFriend",
+  userController.user_update_add_friendRequest
+);
 
 module.exports = router;
