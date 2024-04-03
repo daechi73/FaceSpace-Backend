f89c48733aa356ccc6f1e679df834ed225613de4
J <59581793+daechi73@users.noreply.github.com>
2024-04-02 23:14:34 -0400

    implemented friendRequest_listAll to return all friendRequest

--- controllers/friendRequestController.js
+++ controllers/friendRequestController.js
@@ -1,6 +1,8 @@
-const Friend = require("../models/FriendRequest");
+const FriendReq = require("../models/FriendRequest");
 const asyncHandler = require("express-async-handler");
 
 exports.friendRequest_listAll = asyncHandler(async (req, res, next) => {
-  res.json("working");
+  const friendReq = await FriendReq.find().exec();
+  if (friendReq.length === 0) return res.json("Friend Request empty");
+  return res.json(friendReq);
 });
