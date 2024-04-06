4ad4c718725f3523884500d3af7edc1d405aa875
J <59581793+daechi73@users.noreply.github.com>
2024-04-05 21:58:46 -0400

    routed post friend_request_delete

--- routes/friendRequests.js
+++ routes/friendRequests.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const fReqController = require("../controllers/friendRequestController");
 
 router.get("/", fReqController.friendRequest_listAll);
+router.post("/:id/delete", fReqController.friendRequest_delete);
 
 module.exports = router;
