5f7d7d6c59c6f106e2610229dbb24cc7e527b008
J <59581793+daechi73@users.noreply.github.com>
2024-04-06 20:35:14 -0400

    post_post_posts addes post to the posted user's post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Post = require("../models/Post");
+const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
 exports.posts_get_posts = asyncHandler(async (req, res, next) => {
@@ -23,6 +24,7 @@ exports.posts_post_posts = [
     .withMessage("You must have some input to submit a post"),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
+    const postedUser = await User.findById(req.body.user.id);
     if (!errors.isEmpty()) {
       return res.json({
         status: "failure",
@@ -33,7 +35,9 @@ exports.posts_post_posts = [
       posted_user: req.body.user,
       post_content: req.body.post,
     });
-    await post.save();
+    postedUser.posts.push(post);
+    console.log(postedUser);
+    await Promise.all([post.save(), postedUser.save()]);
     return res.json({ status: "success", msg: "Post successfully added" });
   }),
 ];
