ebdf6a3356665549f68f1b834cc829eeb706a556
J <59581793+daechi73@users.noreply.github.com>
2024-04-06 20:34:36 -0400

    routed user_get_details_with_username

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,11 @@ const userController = require("../controllers/usersController");
 
 router.get("/", userController.user_get_users);
 router.get("/sign_out", userController.user_sign_out);
-router.get("/:id", userController.user_get_user_detail);
+router.get(
+  "/username/:username",
+  userController.user_get_user_detail_with_username
+);
+router.get("/:id", userController.user_get_user_detail_with_id);
 router.post("/other_users", userController.user_get_other_users);
 router.post("/sign_in", userController.user_sign_in);
 router.post("/sign_up", userController.user_sign_up);
