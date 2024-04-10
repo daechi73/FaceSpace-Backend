96bd3f6b9f5096d3ced2c7797881c29fa40f4c56
J <59581793+daechi73@users.noreply.github.com>
2024-04-09 22:52:38 -0400

    implemented chatbox_add_message middleware

--- controllers/ChatBoxController.js
+++ controllers/ChatBoxController.js
@@ -12,3 +12,27 @@ exports.chatbox_get_all = asyncHandler(async (req, res, next) => {
     msg: "Retreiving all chatboxes success",
   });
 });
+
+exports.chatbox_add_message = [
+  asyncHandler(async (req, res, next) => {
+    const existingChatbox = await ChatBox.find({
+      users: { $in: [req.body.message.sender, req.body.message.receiver] },
+    });
+    if (existingChatbox !== null || existingChatbox !== undefined) {
+      existingChatbox.messages.push(message);
+      await existingChatbox.save();
+      return res.json({ status: "success", msg: "added messages to chatbox" });
+    }
+    const users = [req.body.message.sender, req.body.message.receiver];
+    const chatbox = new ChatBox({
+      users: users,
+      messages: req.body.message,
+    });
+    await chatbox.save();
+    return res.json({
+      status: "success",
+      chatbox: chatbox,
+      msg: "chatbox created successfully",
+    });
+  }),
+];
