b3c11d7ea2b21e40a297ea0fa0a776aebbea05ce
J <59581793+daechi73@users.noreply.github.com>
2024-04-10 23:47:56 -0400

    routed chatbox_add_message

--- routes/chatboxes.js
+++ routes/chatboxes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const chatboxController = require("../controllers/ChatBoxController");
 
 router.get("/", chatboxController.chatbox_get_all);
+router.post("/add_messages", chatboxController.chatbox_add_message);
 
 module.exports = router;
