bfc14f536711259f05c280f52421f7af4dd88c38
J <59581793+daechi73@users.noreply.github.com>
2024-04-10 23:48:16 -0400

    routed messages_create

--- routes/messages.js
+++ routes/messages.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const messagesController = require("../controllers/MessagesController");
 
 router.get("/", messagesController.message_get_all);
+router.post("/create", messagesController.message_create);
 
 module.exports = router;
