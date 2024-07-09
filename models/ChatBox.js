ffb8a78af09a2464346a953f7806ed8eb7e132e9
J <59581793+daechi73@users.noreply.github.com>
2024-07-09 15:25:27 -0400

    changed new_message property into string type

--- models/ChatBox.js
+++ models/ChatBox.js
@@ -5,7 +5,7 @@ const ChatBoxSchema = new Schema({
   users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
   date_created: { type: Date, default: Date.now },
   messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
-  new_message: { type: Boolean, default: true },
+  new_message: { type: String },
 });
 
 ChatBoxSchema.virtual("url").get(function () {
@@ -13,5 +13,5 @@ ChatBoxSchema.virtual("url").get(function () {
 });
 
 ChatBoxSchema.set("toJSON", { virtuals: true });
-
+``;
 module.exports = mongoose.model("ChatBox", ChatBoxSchema);
