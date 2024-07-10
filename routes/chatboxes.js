45d42ec350f75690f43c7831ca587ddbd217971f
J <59581793+daechi73@users.noreply.github.com>
2024-07-10 15:58:11 -0400

    routed chatbox_update_new_message

--- routes/chatboxes.js
+++ routes/chatboxes.js
@@ -3,5 +3,9 @@ const chatboxController = require("../controllers/ChatBoxController");
 
 router.get("/", chatboxController.chatbox_get_all);
 router.post("/add_messages", chatboxController.chatbox_add_message);
+router.post(
+  "/:id/update/new_message",
+  chatboxController.chatbox_update_newMessage
+);
 
 module.exports = router;
