0286d15cec1f8224b7d14c4d689653e02aa6ed05
J <59581793+daechi73@users.noreply.github.com>
2024-08-05 22:20:08 -0400

    renamed posts_post_posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const postsController = require("../controllers/postsController");
 
 router.get("/", postsController.posts_get_posts);
-router.post("/post", postsController.posts_post_posts);
+router.post("/post", postsController.posts_post_posts_main);
 
 module.exports = router;
