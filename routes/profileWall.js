60a78e5c0943abc67174fe5ea30d7818825d5cac
J <59581793+daechi73@users.noreply.github.com>
2024-09-02 23:37:04 -0400

    profilewall_delete_post routed

--- routes/profileWall.js
+++ routes/profileWall.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const profileWallController = require("../controllers/ProfileWallController");
 
 router.get("/", profileWallController.profileWall_get_all);
+router.get("/:id", profileWallController.profileWall_get_single_with_id);
+
 router.post("/create", profileWallController.profileWall_create);
+router.post("/:id/delete", profileWallController.profileWall_delete_post);
 
 module.exports = router;
