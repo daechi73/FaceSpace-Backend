ae05cf43672532b4b6d53af68b4eda4d1730ee6d
J <59581793+daechi73@users.noreply.github.com>
2024-09-12 18:29:55 -0400

    removed dev console.log

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -121,7 +121,6 @@ exports.posts_delete_post = asyncHandler(async (req, res, next) => {
     const post = await Post.findById(req.params.id).exec();
     if (post === null) return console.log("post not found");
 
-    console.log(req.params.id);
     await Post.findByIdAndDelete(req.params.id);
     return res.json({
       status: "Success",
