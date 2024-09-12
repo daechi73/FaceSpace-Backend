ae51370fc3b3429b232360796f4c04f5276aa975
J <59581793+daechi73@users.noreply.github.com>
2024-09-12 17:52:30 -0400

    routed posts_get_post & posts_post_delete

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const postsController = require("../controllers/postsController");
 
 router.get("/", postsController.posts_get_posts);
+router.get("/:id", postsController.posts_get_post);
+
 router.post("/postMain", postsController.posts_post_posts_main);
 router.post("/postProfileWall", postsController.posts_post_posts_profileWall);
+router.post("/:id/delete", postsController.posts_post_delete);
 
 module.exports = router;
