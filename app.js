7398e17cab66d72fcd1d313bc2bb91966437a00f
J <59581793+daechi73@users.noreply.github.com.>
2024-10-11 23:44:23 -0400

    changed access control allow origin cors setting to production mode

--- app.js
+++ app.js
@@ -63,9 +63,9 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
 
   //production mode
-  res.setHeader("Access-Control-Allow-Origin", "https://daechi73.github.io/FaceSpace/");
+  res.setHeader("Access-Control-Allow-Origin", "https://daechi73.github.io/");
   //dev mode
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
