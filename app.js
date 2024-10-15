aca30b9fcc92aeb6fa4ae5026c9cb8fca75ec94a
J <59581793+daechi73@users.noreply.github.com.>
2024-10-15 19:48:11 -0400

    changed cors acao to https://facespace.onrender.com/

--- app.js
+++ app.js
@@ -64,10 +64,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
 
-  // //production mode
-  // res.setHeader("Access-Control-Allow-Origin", "https://daechi73.github.io");
+  //  production mode render
+
+
+  // //production mode github
+  res.setHeader("Access-Control-Allow-Origin", "https://facespace.onrender.com");
   //dev mode
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:4173");
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
