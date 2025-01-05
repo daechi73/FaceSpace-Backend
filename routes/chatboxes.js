e92521d044870c20cc1205cb5f6ce45ba00f0048
J <59581793+daechi73@users.noreply.github.com.>
2025-01-04 20:25:16 -0500

    chatbox_live routed

--- routes/chatboxes.js
+++ routes/chatboxes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const chatboxController = require("../controllers/ChatBoxController");
 
 router.get("/", chatboxController.chatbox_get_all);
+router.get("/liveChat", chatboxController.chatbox_live);
 router.post("/add_messages", chatboxController.chatbox_add_message);
 router.post(
   "/:id/update/new_message",
